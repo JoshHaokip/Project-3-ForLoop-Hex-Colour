64ef4baadf612d318cbc836f27eb59d6277ef67d
JoshHaokip <joshhaokip@gmail.com>
2020-06-27 19:18:13 +0100

    First upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hexBtn = document.querySelector('.hexBtn');
+const hexBackground = document.querySelector('body');
+const hex = document.querySelector('.hexCode');
+
+hexBtn.addEventListener('click', getHex); 
+
+
+function getHex () {
+
+    let hexCol = '#'; 
+
+    for (let i = 0; i < 6; i++){
+
+        let random = Math.floor(Math.random() * hexNumbers.length)
+        hexCol += hexNumbers[random]; 
+    }
+
+    hexBackground.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+
+}
